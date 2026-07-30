46f4e8bf7c16b7ace6bdc139117b0d93493fe3cd
Prakhar Saini <prakharsaini786@gmail.com>
2026-07-30 15:03:11 +0530

    chore: add next-env.d.ts for next.js global type declarations and css imports

---
+++ next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited manually.
+// Next.js automatically generates this file when running `next dev` or `next build`.
